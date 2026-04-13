4cb54b79c7c69cd0439fcdc1c65e7b1583b677f6
Sathya1245632 <sksathya009@gmail.com>
2026-04-14 02:44:55 +0530

    Initial commit for EliteTrades project

---
+++ project/src/App.tsx
@@ -0,0 +1,18 @@
+export function App() {
+  return (
+    <iframe
+      src="/elitetrades.html"
+      title="EliteTrades"
+      style={{
+        position: 'fixed',
+        inset: 0,
+        width: '100%',
+        height: '100%',
+        border: 'none',
+        display: 'block',
+      }}
+    />
+  )
+}
+
+export default App
